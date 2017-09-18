ae19d1b321a0edf3a3257e194df7be1d0fc4be77
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 00:28:35 -0700

    Instructor note

--- mktmpfiles.sh
+++ mktmpfiles.sh
@@ -10,3 +10,12 @@ exit 0
 
 # used https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/3/html/Step_by_Step_Guide/s1-navigating-cd.html
 # to learn how to use cd ..  for past directory
+
+
+###
+# INSTRUCTOR NOTE:
+#
+# The mkdir command above creates directories. The assignment requested blank files.
+# Blank files can be easily created with "touch" instead of "mkdir".
+# You also have a stray fibs.py file in your repository. Otherwise good.
+###
